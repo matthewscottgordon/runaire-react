ee12f65a0d30d3d4d1559e39d3914f8cc39b8ee6
Matthew Gordon <matthew@gordon.earth>
2021-06-05 14:58:26 -0400

    Call API to load list when app starts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
-import React, { useState } from "react";
+import 'regenerator-runtime/runtime'
+import React, { useState, useEffect } from 'react'
 
-export default function App() {
-    const [items, setItems] = useState([]);
-    if(items.length == 0)
-        return "No Items.";
-    return <DayList items={items} />;
+export default function App () {
+    const [items, setItems] = useState([])
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        fetchDayList()
+            .then(setItems)
+            .catch(err => setError(true))
+    }, [])
+
+    if (error) { return 'ERROR' }
+    if (items.length == 0) { return 'No Items.' }
+    return <DayList items={items} />
 }
 
-function DayList({items}) {
-    return(
-        <ul>
-          {items.map((item, i) => (
-              <DayListItem key={i} {...item} />
-          ))}
+function DayList ({ items }) {
+    return (
+            <ul>
+            {items.map((item, i) => (
+                    <DayListItem key={i} {...item} />
+            ))}
         </ul>
-    );
+    )
 }
 
-function DayListItem({name, is_done}) {
-    return(
-        <li>
-          <input type="checkbox" checked={is_done}/>
-          {name}
-        </li>);
+function DayListItem ({ name, is_done }) {
+    return (
+            <li>
+            <input type='checkbox' checked={is_done} />
+            {name}
+        </li>
+    )
 }
 
-async function fetchDayList() {
-    const response = await fetch('/api/list');
-    return await response.json();
+async function fetchDayList () {
+    const response = await fetch('/api/list')
+    return await response.json()
 };
-
-function render(json) {
-    ReactDOM.render(
-        <DayList items={json} />,
-        document.getElementById("root"));
-};
-
-//fetchDayList()
-//    .then(render)
-//    .catch(err => {
-//        ReactDOM.render("Error", document.getElementById("root"));
-//        console.log(err);
-//    });
